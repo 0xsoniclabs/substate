996f529494563dd526b7f82e10864b09340c9067
Eittipat.K <iammop@gmail.com>
2025-03-11 23:59:54 +0700

    Enhance parseBlockSegment to support SI units for both first and last block numbers

--- cmd/rlp-to-protobuf/utils.go
+++ cmd/rlp-to-protobuf/utils.go
@@ -19,31 +19,46 @@ func newBlockSegment(first, last uint64) *blockSegment {
 }
 
 // parseBlockSegment parses a string representation of a block segment into a blockSegment struct.
-// The string can be in the format "first" or "first-last" with optional SI units (k for 1000, M for 1000000).
-// If the last block number is not provided, it is assumed to be the same as the first block number.
+// The string can be in the following formats:
+//   - Single block: "1", "10", "1k" (1000), "1M" (1000000)
+//   - Block range: "1-10", "1k-5k" (1000-5000), "1k-1M" (1000-1000000), "1-1M" (1-1000000)
+//
+// SI units can be applied to either number independently:
+//   - k: multiply by 1,000 (kilo)
+//   - M: multiply by 1,000,000 (mega)
 //
 // Parameters:
-// - s: The string representation of the block segment.
+//   - s: The string representation of the block segment.
 //
 // Returns:
-// - A pointer to a blockSegment struct with the parsed first and last block numbers.
-// - An error if the string is not in a valid format or if the block numbers are invalid.
+//   - A pointer to a blockSegment struct with the parsed first and last block numbers.
+//   - An error if the string is not in a valid format or if the block numbers are invalid.
 func parseBlockSegment(s string) (*blockSegment, error) {
 	var err error
-	// <first>: first block number
-	// <last>: optional, last block number
-	// <siunit>: optinal, k for 1000, M for 1000000
-	re := regexp.MustCompile(`^(?P<first>[0-9][0-9_]*)((-|~)(?P<last>[0-9][0-9_]*)(?P<siunit>[kM]?))?$`)
+	// <first>: first block number with optional SI unit (k for 1000, M for 1000000)
+	// <last>: optional, last block number with optional SI unit
+	re := regexp.MustCompile(`^(?P<first>[0-9][0-9_]*)(?P<firstunit>[kM]?)((-|~)(?P<last>[0-9][0-9_]*)(?P<lastunit>[kM]?))?$`)
 	seg := &blockSegment{}
 	if !re.MatchString(s) {
 		return nil, fmt.Errorf("invalid block segment string: %q", s)
 	}
+
 	matches := re.FindStringSubmatch(s)
 	first := strings.ReplaceAll(matches[re.SubexpIndex("first")], "_", "")
 	seg.First, err = strconv.ParseUint(first, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("invalid block segment first: %s", err)
 	}
+
+	// Apply SI unit for first number
+	firstUnit := matches[re.SubexpIndex("firstunit")]
+	switch firstUnit {
+	case "k":
+		seg.First = seg.First * 1_000
+	case "M":
+		seg.First = seg.First * 1_000_000
+	}
+
 	last := strings.ReplaceAll(matches[re.SubexpIndex("last")], "_", "")
 	if len(last) == 0 {
 		seg.Last = seg.First
@@ -52,18 +67,19 @@ func parseBlockSegment(s string) (*blockSegment, error) {
 		if err != nil {
 			return nil, fmt.Errorf("invalid block segment last: %s", err)
 		}
+
+		// Apply SI unit for last number
+		lastUnit := matches[re.SubexpIndex("lastunit")]
+		switch lastUnit {
+		case "k":
+			seg.Last = seg.Last * 1_000
+		case "M":
+			seg.Last = seg.Last * 1_000_000
+		}
 	}
-	siunit := matches[re.SubexpIndex("siunit")]
-	switch siunit {
-	case "k":
-		seg.First = seg.First*1_000 + 1
-		seg.Last = seg.Last * 1_000
-	case "M":
-		seg.First = seg.First*1_000_000 + 1
-		seg.Last = seg.Last * 1_000_000
-	}
+
 	if seg.First > seg.Last {
-		return nil, fmt.Errorf("block segment first is larger than last: %v-%v", seg.First, seg.Last)
+		return nil, fmt.Errorf("block segment first (%v) is greater than last (%v)", seg.First, seg.Last)
 	}
 	return seg, nil
 }
--- cmd/rlp-to-protobuf/utils_test.go
+++ cmd/rlp-to-protobuf/utils_test.go
@@ -9,10 +9,16 @@ import (
 func TestParseBlockSegment(t *testing.T) {
 	// success cases
 	mapInputOutput := map[string]*blockSegment{
-		"1":     newBlockSegment(1, 1),
-		"0-1M":  newBlockSegment(1, 1000000),
-		"1-200": newBlockSegment(1, 200),
-		"0-1k":  newBlockSegment(1, 1000),
+		"1":        newBlockSegment(1, 1),
+		"1k":       newBlockSegment(1000, 1000),
+		"0-1M":     newBlockSegment(0, 1000000),
+		"1-200":    newBlockSegment(1, 200),
+		"0-1k":     newBlockSegment(0, 1000),
+		"1k-1M":    newBlockSegment(1000, 1000000),
+		"1M-2M":    newBlockSegment(1000000, 2000000),
+		"10-100":   newBlockSegment(10, 100),
+		"1k-5k":    newBlockSegment(1000, 5000),
+		"5k-10000": newBlockSegment(5000, 10000),
 	}
 
 	for input, expected := range mapInputOutput {
@@ -23,9 +29,10 @@ func TestParseBlockSegment(t *testing.T) {
 
 	// error cases
 	mapInputOutputError := map[string]string{
-		"1-0":                      "block segment first is larger than last: 1-0",
-		"1M":                       "invalid block segment string: \"1M\"",
-		"1-1M":                     "block segment first is larger than last: 1000001-1000000",
+		"1-0":                      "block segment first (1) is greater than last (0)",
+		"1K":                       "invalid block segment string: \"1K\"",
+		"1K-1M":                    "invalid block segment string: \"1K-1M\"",
+		"1k-1G":                    "invalid block segment string: \"1k-1G\"",
 		"1-100000000000000000000M": "invalid block segment last: strconv.ParseUint: parsing \"100000000000000000000\": value out of range",
 		"900000000000000000000-900000000000000000000M": "invalid block segment first: strconv.ParseUint: parsing \"900000000000000000000\": value out of range",
 	}
