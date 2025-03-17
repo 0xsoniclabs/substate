d073c8e91169358ee7e567f8614e38af59a745be
Eittipat.K <iammop@gmail.com>
2025-03-17 16:39:37 +0700

    Add rlp-to-protobuf command (#5)

---
+++ cmd/rlp-to-protobuf/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:   "rlp-to-protobuf",
+		Usage:  "Convert rlp encoded substate to protobuf encoded substate",
+		Action: RunRlpToProtobuf,
+		Flags: []cli.Flag{
+			&WorkersFlag,
+			&SrcDbFlag,
+			&DstDbFlag,
+			&SkipTransferTxsFlag,
+			&SkipCallTxsFlag,
+			&SkipCreateTxsFlag,
+			&BlockSegmentFlag,
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/rlp-to-protobuf/rlp_to_protobuf.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/syndtr/goleveldb/leveldb/opt"
+
+	"github.com/0xsoniclabs/substate/db"
+	"github.com/0xsoniclabs/substate/substate"
+	"github.com/urfave/cli/v2"
+)
+
+type rlpToProtobufCommand struct {
+	src db.SubstateDB
+	dst db.SubstateDB
+	ctx *cli.Context
+}
+
+func (c *rlpToProtobufCommand) execute() error {
+
+	segment, err := parseBlockSegment(c.ctx.String(BlockSegmentFlag.Name))
+	if err != nil {
+		return err
+	}
+
+	taskPool := &db.SubstateTaskPool{
+		Name:     "rlp-to-protobuf",
+		TaskFunc: c.performSubstateUpgrade,
+
+		First: segment.First,
+		Last:  segment.Last,
+
+		Workers:         c.ctx.Int(WorkersFlag.Name),
+		SkipTransferTxs: c.ctx.Bool(SkipTransferTxsFlag.Name),
+		SkipCallTxs:     c.ctx.Bool(SkipCallTxsFlag.Name),
+		SkipCreateTxs:   c.ctx.Bool(SkipCreateTxsFlag.Name),
+
+		Ctx: c.ctx,
+
+		DB: c.src,
+	}
+	err = c.dst.SetSubstateEncoding("protobuf")
+	if err != nil {
+		return err
+	}
+	return taskPool.Execute()
+}
+
+func (c *rlpToProtobufCommand) performSubstateUpgrade(
+	block uint64,
+	tx int,
+	substate *substate.Substate,
+	taskPool *db.SubstateTaskPool,
+) error {
+	err := c.dst.PutSubstate(substate)
+	if err != nil {
+		return fmt.Errorf("failed to put substate: %w", err)
+	}
+	return nil
+}
+
+func RunRlpToProtobuf(ctx *cli.Context) error {
+	// Open old DB
+	src, err := db.NewSubstateDB(ctx.String(SrcDbFlag.Name), &opt.Options{
+		OpenFilesCacheCapacity: 1024,
+		BlockCacheCapacity:     50 * opt.MiB,
+		WriteBuffer:            25 * opt.MiB,
+		ReadOnly:               true,
+	}, nil, nil)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Open new DB
+	dst, err := db.NewSubstateDB(ctx.String(DstDbFlag.Name), &opt.Options{
+		OpenFilesCacheCapacity: 1024,
+		BlockCacheCapacity:     50 * opt.MiB,
+		WriteBuffer:            25 * opt.MiB,
+		ReadOnly:               false,
+	}, nil, nil)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	command := rlpToProtobufCommand{
+		src: src,
+		dst: dst,
+		ctx: ctx,
+	}
+	return command.execute()
+}
